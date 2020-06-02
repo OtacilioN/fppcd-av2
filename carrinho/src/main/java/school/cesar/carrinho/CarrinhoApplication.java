c47217ae1983e45f26a8ddf461293f9a1ccafa30
OtacilioN <contato@otaciliomaia.com>
2020-06-02 03:54:35 -0300

    Add success and fail examples

--- carrinho/src/main/java/school/cesar/carrinho/CarrinhoApplication.java
+++ carrinho/src/main/java/school/cesar/carrinho/CarrinhoApplication.java
@@ -16,32 +16,37 @@ public class CarrinhoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarrinhoApplication.class, args);
 		try {
-			requestEstoque();
+			// Example of failing by out of stock book
+			if (requestEstoque("[\"JAVA-BOOK-100-PAGES\"]")) {
+				System.out.println("Could get JAVA-BOOK-100-PAGES from stock");
+			} else {
+				System.out.println("Fail to get JAVA-BOOK-100-PAGES from stock");
+			}
+
+			// Example of available book in stock
+			if (requestEstoque("[\"JAVA-BOOK-300-PAGES\"]")) {
+				System.out.println("Could get JAVA-BOOK-300-PAGES from stock");
+			} else {
+				System.out.println("Fail to get JAVA-BOOK-300-PAGES from stock");
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	private static void requestEstoque() throws Exception {
-
+	private static boolean requestEstoque(String book) throws Exception {
+		String result = "";
 		HttpPost post = new HttpPost("http://localhost:8080");
 		post.addHeader("content-type", "application/json");
-
-		// add request parameter, form parameters
-		// List<NameValuePair> urlParameters = new ArrayList<>();
-		// urlParameters.addAll(["JAVA-BOOK-300-PAGES"]);
-		// urlParameters.add(new BasicNameValuePair("username", "abc"));
-		// urlParameters.add(new BasicNameValuePair("password", "123"));
-		// urlParameters.add(new BasicNameValuePair("custom", "secret"));
 		StringBuilder json = new StringBuilder();
-		json.append("[\"JAVA-BOOK-300-PAGES\"]");
+		json.append(book);
 
 		post.setEntity(new StringEntity(json.toString()));
 		try (CloseableHttpClient httpClient = HttpClients.createDefault();
 				CloseableHttpResponse response = httpClient.execute(post)) {
-
-			System.out.println(EntityUtils.toString(response.getEntity()));
+			result = EntityUtils.toString(response.getEntity());
 		}
+		return result.indexOf("\"success\":\"true\"") != -1 ? true : false;
 	}
 }
